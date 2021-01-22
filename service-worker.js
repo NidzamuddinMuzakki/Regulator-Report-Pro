4afecb60586c67d8a72194580532ed9da8c9fd3a
Nidzamuddin <thealjabar@gmail.com>
2021-01-22 18:20:19 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Regulator-Report-Pro/precache-manifest.7267f8a6577a218082b95463e00ac2bf.js"
+  "RegulatorReport.github.io/precache-manifest.9cdcabb2fca55381140e822cc7c42ba0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Regulator-Report-Pro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("RegulatorReport.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
